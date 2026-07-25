64e8756dd0bc8b0b94af2bb7048723b2258f7e09
vortex <ffvortex20@gmail.com>
2026-07-25 14:23:56 -0300

    Fix export statement formatting in firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,8 +1,6 @@
-// Importações dos módulos do Firebase (versão modular compatível com Web)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
-// Configuração extraída do seu google-services (6).json
 const firebaseConfig = {
   apiKey: "AIzaSyACGn5I--vTEmJ6eijN2lytZiMgo2ElnW4",
   databaseURL: "https://vortexdev2-default-rtdb.firebaseio.com",
@@ -10,7 +8,6 @@ const firebaseConfig = {
   storageBucket: "vortexdev2.firebasestorage.app"
 };
 
-// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
